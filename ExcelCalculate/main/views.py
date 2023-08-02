7ea6e89efed1f4b82672ac539f059861e442fa7e
FORKY <infoker12@naver.com>
2023-08-02 18:17:09 +0900

    update file
    2023.08.02 엑셀파일 읽어오기 구현

--- ExcelCalculate/main/views.py
+++ ExcelCalculate/main/views.py
@@ -5,7 +5,15 @@ from sendEmail.views import *
 
 # Create your views here.
 def index(request):
-    return render(request, "main/index.html")
+    # 로그인된 사용자만 접근
+    # 조건문 : 사용자의 정보가 세션에 존재하면 메인화면으로 출력,
+    # 만약, 사용자의 정보가 세션에 없다면 로그인 화면으로 출력
+    if 'user_name' in request.session.keys():
+        return render(request, "main/index.html") # 사용자의 세션정보가 담긴 상태의 index.html
+    else:
+        return redirect('main_signin')
+
+    # return render(request, "main/index.html") # 아무런 세션 정보가 없는 index.html
 
 def signup(request):
     return render(request, "main/signup.html")
@@ -43,7 +51,32 @@ def join(request):
 
 
 def signin(request):
+    
     return render(request, "main/signin.html")
+
+def login(request):
+    # 로그인된 사용자만 이용할 수 있도록 구현
+    # 이 때, 현재 사용자가 로그인된 사용자인지 판단하기 위해 세션사용 FROM (verify 함수에서 만든 세션)
+    # 세션 처리 진행함
+    # 각각 독립된 객체로 진행됨
+
+    loginEmail = request.POST['loginEmail']
+    # html의 name이 loginEmail이라서 사용함
+    loginPW = request.POST['loginPW']
+    user = User.objects.get(user_email=loginEmail)
+
+    # 회원가입시 입력한 패스워드와 입력한 패스워드가 같은지 확인
+    if user.user_password == loginPW:
+        request.session['user_name'] = user.user_name # 사용자가 회원가입 시, 입력한 정보
+        request.session['user_email'] = user.user_email # 사용자가 회원가입 시, 입력한 정보
+        return redirect('main_index')
+    
+    else:
+        # 로그인 실패, 정보가 다름
+        return redirect("main_loinFail")
+
+    # return None
+    #None으로 표시를 해도 오류 안남, HttpResponse("확인문구")로 해도 됨 상관없음
 
 def verifyCode(request):
     return render(request, "main/verifyCode.html")
@@ -83,4 +116,16 @@ def verify(request):
     # return redirect("main_index") # 인증이 완료되면 메인화면으로 보내라
 
 def result(request):
-    return render(request, "main/result.html")
+    if 'user_name' in request.session.keys():
+        return render(request, 'main/result.html') # 사용자의 세션 정보가 담겨져 있는 상태에서의 index.html
+    else:
+        return redirect("main_signin")
+    
+
+def logout(request):
+    # 로그아웃의 개념 : 세션 정보를 삭제하는 것
+    # 파이썬에서 객체를 지울 때
+    del request.session['user_name']
+    del request.session['user_email']
+
+    return redirect('main_signin')
