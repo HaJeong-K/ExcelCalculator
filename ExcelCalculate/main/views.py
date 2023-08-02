57ee247fd96305fc0cc85416e55555b26621aa63
FORKY <infoker12@naver.com>
2023-08-02 09:32:38 +0900

    update file
    2023.08.01 회원가입 인증코드 구현

--- ExcelCalculate/main/views.py
+++ ExcelCalculate/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
+from random import *
+from sendEmail.views import *
 
 # Create your views here.
 def index(request):
@@ -9,7 +11,7 @@ def signup(request):
     return render(request, "main/signup.html")
 
 def join(request):
-    print(request)
+    print("테스트", request)
 
     name = request.POST['signupName']
     email = request.POST['signupEmail']
@@ -19,7 +21,26 @@ def join(request):
 
     print("사용자 정보 저장 완료됨!!")
 
-    return redirect("main_verifyCode")
+    # 인증코드 하나 생성
+    code = randint(1000, 9000)
+    print("인증코드 생성--------------------", code) # 서버가 보낸 코드, 쿠키와 세션
+
+    response = redirect("main_verifyCode") # 응답을 객체로 저장한다
+    response.set_cookie('code', code) # 인증코드
+    response.set_cookie('user_id', user.id)
+
+    print("응답 객체 완성--------------------", response)
+
+    # 이메일 발송하는 함수 만들어보기
+    # 이메일 주소 2개 준비하기
+    send_result = send(email, code)
+    if send_result:
+        print("Main > views.py > 이메일 발송 중...")
+        return response
+    else:
+        return HttpResponse("이메일 발송 실패")
+
+
 
 def signin(request):
     return render(request, "main/signin.html")
@@ -28,7 +49,38 @@ def verifyCode(request):
     return render(request, "main/verifyCode.html")
 
 def verify(request):
-    return redirect("main_index") # 인증이 완료되면 메인화면으로 보내라
+    # 사용자가 입력한 code값을 받아야 함
+    user_code = request.POST['verifyCode']
+
+    # 쿠키에 저장된 code값과 매칭시킴 (join 함수 확인)
+    cookie_code = request.COOKIES.get('code')
+    print("코드 확인: ", user_code, cookie_code)
+
+    if user_code == cookie_code:
+        user = User.objects.get(id=request.COOKIES.get('user_id')) #SELECT FROM WHERE id = cookie_id 데이터를 가져오는 것.
+        user.user_validate = 1 # True=1 / False=0
+        user.save()
+    
+        print("DB에 user_validate 업데이트---------------------")
+
+        response = redirect('main_index')
+        # 저장되어 있는 쿠키 삭제
+        response.delete_cookie('code')
+        response.delete_cookie('user_id')
+        # response.set_cookie('user',user)
+
+        # 사용자 정보를 세션에 저장
+        request.session['user_name'] = user.user_name # 로그인 화면 구현
+        request.session['user_name'] = user.user_email # 로그인 화면 구현
+        return response
+
+
+    else :
+        print("False")
+        return redirect("main_verifyCode") # verifyCode 화면으로 돌림
+    
+
+    # return redirect("main_index") # 인증이 완료되면 메인화면으로 보내라
 
 def result(request):
     return render(request, "main/result.html")
