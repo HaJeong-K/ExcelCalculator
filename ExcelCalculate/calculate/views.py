adaf17ce72f31cc97f306e35b34af7059c29ffd2
FORKY <infoker12@naver.com>
2023-08-02 09:26:46 +0900

    Delete ExcelCalculate directory

--- ExcelCalculate/calculate/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def calculate(request):
-    return HttpResponse("calculate, calculate function!")
